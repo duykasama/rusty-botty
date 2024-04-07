5e9695823de264b5e4c93000f9c913bd68e9756a
duykasama <duykasama@gmail.com>
2024-04-07 19:02:33 +0700

    init: setup project

---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use serenity::{all::{EventHandler, GatewayIntents, Message}, prelude::Context, async_trait, Client};
+use std::env;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+        if msg.content == "!ping" {
+            if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+            }
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = env::var("DISCORD_TOKEN")
+        .expect("Expected a token in the environment");
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+    
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
+}
