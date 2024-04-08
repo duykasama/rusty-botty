c16c3e3e08c5cd627f04c35f552cf118863fab73
duykasama <duykasama@gmail.com>
2024-04-08 12:55:39 +0700

    feat: slash command

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
-use serenity::{all::{EventHandler, GatewayIntents, Message}, prelude::Context, async_trait, Client};
+use serenity::{all::{EventHandler, GatewayIntents, Message}, async_trait, Client};
 use std::env;
 
 struct Handler;
+struct Data {}
+type Error = Box<dyn std::error::Error + Send + Sync>;
+type Context<'a> = poise::Context<'a, Data, Error>;
 
 #[async_trait]
 impl EventHandler for Handler {
-    async fn message(&self, ctx: Context, msg: Message) {
+    async fn message(&self, ctx: serenity::prelude::Context, msg: Message) {
         if msg.content == "!ping" {
             if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
                 println!("Error sending message: {:?}", why);
             }
         }
     }
+}
+
+#[poise::command(slash_command, prefix_command)]
+async fn age(
+    ctx: Context<'_>,
+    #[description = "Selected user"] user: Option<poise::serenity_prelude::User>,
+) -> Result<(), Error> {
+    let user = user.as_ref().unwrap_or_else(|| ctx.author());
+    let response = format!("{}'s account was created at {}", user.name, user.created_at());
+    ctx.say(response).await?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -22,7 +36,23 @@ async fn main() {
         | GatewayIntents::DIRECT_MESSAGES
         | GatewayIntents::MESSAGE_CONTENT;
 
-    let mut client = Client::builder(&token, intents).event_handler(Handler).await.expect("Error creating client");
+    let framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![age()],
+            ..Default::default()
+        })
+        .setup(|ctx, _ready, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
+    let mut client = Client::builder(&token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await.expect("Error creating client");
     
     if let Err(why) = client.start().await {
         println!("Client error: {:?}", why);
